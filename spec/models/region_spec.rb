6fdb86f0f37d72cab0828e54813d575178db0106
Nikita Rubocki <nikita.rubocki@gmail.com>
2021-02-09 21:45:38 -0800

    Add method tests

--- spec/models/region_spec.rb
+++ spec/models/region_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Region, type: :model do
 
-  let (:region) { Region.new() }
+  let (:region) { create(:region) }
 
   it 'has a name' do
     expect(region).to respond_to(:name)
@@ -17,5 +17,15 @@ RSpec.describe Region, type: :model do
     it { should validate_length_of(:name).is_at_least(1).is_at_most(255).on(:create) }
     it { should validate_uniqueness_of(:name).case_insensitive}
   end
+
+  describe '#' do
+    it 'returns a ResourceCategory with the name Unspecified' do
+      expect(ResourceCategory.unspecified.name).to eq('Unspecified')
+    end
+  
+    it 'it has a string representation that is its name' do
+      expect(Region.new(name: 'Fake').to_s).to eq('Fake')
+    end
+  end  
 
 end
