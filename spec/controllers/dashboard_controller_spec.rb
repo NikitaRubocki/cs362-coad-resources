6b7b312817625cfbe75fa689644f15ef4e96f97d
Nikita Rubocki <nikita.rubocki@gmail.com>
2021-03-11 14:39:11 -0800

    Add controller test for dashboard

--- spec/controllers/dashboard_controller_spec.rb
+++ spec/controllers/dashboard_controller_spec.rb
@@ -2,4 +2,11 @@ require 'rails_helper'
 
 RSpec.describe DashboardController, type: :controller do
 
+	context 'unauthenticated user' do
+		it 'redirects to sign_in screen' do
+			get :index
+			expect(response).to redirect_to(new_user_session_url)
+		end
+	end
+
 end
