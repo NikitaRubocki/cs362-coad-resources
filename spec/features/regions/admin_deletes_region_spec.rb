4d9ab80c92dedd8fc164b088ef6b64a357243285
Nikita Rubocki <nikita.rubocki@gmail.com>
2021-03-11 15:41:36 -0800

    Add feature test for deleting regions

--- spec/features/regions/admin_deletes_region_spec.rb
+++ spec/features/regions/admin_deletes_region_spec.rb
@@ -2,4 +2,15 @@ require 'rails_helper'
 
 RSpec.describe 'Deleting a Region', type: :feature do
 
+	let(:admin) { create(:user, :admin_role) }
+
+	it 'deletes a region' do
+		region = create(:region)
+		log_in_as(admin)
+		visit region_path(region)
+		click_on 'Delete'
+		expect(page.body).to have_text('Unspecified')
+		expect(page.body).to have_text('deleted')
+	end
+
 end
