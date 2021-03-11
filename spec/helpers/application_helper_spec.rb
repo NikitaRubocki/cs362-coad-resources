9d369a0940bc600c930f5d0e15c916af52c4e30e
Nikita Rubocki <nikita.rubocki@gmail.com>
2021-03-11 13:23:45 -0800

    Add test coverage for #full_title

--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -2,4 +2,16 @@ require 'rails_helper'
 
 RSpec.describe ApplicationHelper, type: :helper do
 
+	describe '#full_title' do
+		it 'returns default title' do
+			default_title = 'Disaster Resource Network'
+			expect(full_title).to eq(default_title)
+		end
+
+		it 'returns passed title | default title' do
+			title = 'Test | Disaster Resource Network'
+			expect(full_title('Test')).to eq(title)
+		end
+	end
+
 end
