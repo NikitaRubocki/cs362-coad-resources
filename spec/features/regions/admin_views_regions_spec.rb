b51794cafe88f265c623e818dc596a4059bc6d06
Nikita Rubocki <nikita.rubocki@gmail.com>
2021-03-11 15:25:20 -0800

    Change specify to it

--- spec/features/regions/admin_views_regions_spec.rb
+++ spec/features/regions/admin_views_regions_spec.rb
@@ -4,14 +4,14 @@ RSpec.describe 'Viewing Regions', type: :feature do
 
 	let(:admin) { create(:user, :admin_role) }
 
-  specify 'shows a list of region names' do
+  it 'shows a list of region names' do
 		region = create(:region)
 		log_in_as(admin)
 		click_on 'Regions'
 		expect(page.body).to have_text(region.name)
 	end
 
-	specify 'shows the details of one region' do
+	it 'shows the details of one region' do
 		region = create(:region)
 		ticket = create(:ticket, region: region)
 		log_in_as(admin)
