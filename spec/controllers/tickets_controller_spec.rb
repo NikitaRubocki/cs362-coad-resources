c83d010e7dd15a5fccddc51540d7399ca67ddb5f
Nikita Rubocki <nikita.rubocki@gmail.com>
2021-03-11 14:31:59 -0800

    Add controller test for tickets

--- spec/controllers/tickets_controller_spec.rb
+++ spec/controllers/tickets_controller_spec.rb
@@ -2,4 +2,17 @@ require 'rails_helper'
 
 RSpec.describe TicketsController, type: :controller do
 
+	context 'organization user' do
+		before do
+			org_user = build(:user, :org_role)
+			allow(request.env['warden']).to receive(:authenticate!).and_return(org_user)
+			allow(controller).to receive(:current_user).and_return(build(:user, :org_role))
+		end
+		
+		it 'redirects to dashboard' do
+			delete :destroy, params: {id: 'fake'}
+			expect(response).to redirect_to(dashboard_url)
+		end
+	end
+
 end
