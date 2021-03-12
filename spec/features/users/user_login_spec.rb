0a82031fffb7f5fc91ac1756d27dc41378c70bd6
Nikita Rubocki <nikita.rubocki@gmail.com>
2021-03-11 18:12:08 -0800

    Add feature tests for user login

--- spec/features/users/user_login_spec.rb
+++ spec/features/users/user_login_spec.rb
@@ -2,4 +2,15 @@ require 'rails_helper'
 
 RSpec.describe 'Logging in', type: :feature do
 
+	it 'logs a user into the system' do
+		user = create(:user)
+		visit root_path
+		click_on 'Log in'
+		fill_in 'Email address', with: user.email
+		fill_in 'Password', with: user.password
+		click_on 'Sign in'
+		expect(page.body).to have_text('successfully')
+		expect(page.body).to have_text('Dashboard')
+	end
+
 end
