5669beac4bcc79849b78f5d283ab8605d9f025bd
Nikita Rubocki <nikita.rubocki@gmail.com>
2021-03-11 18:12:18 -0800

    Add feature tests for user registration

--- spec/features/users/user_registration_spec.rb
+++ spec/features/users/user_registration_spec.rb
@@ -2,4 +2,16 @@ require 'rails_helper'
 
 RSpec.describe 'User registration', type: :feature do
 
+	it 'registers a user' do
+		visit root_path
+		click_on 'Sign up'
+		fill_in 'Email address', with: 'fake@fake.com'
+		fill_in 'Password', with: 'verysecurepassword'
+		fill_in 'Password confirmation', with: 'verysecurepassword'
+		within 'form#new_user' do
+			click_on 'Sign up' 
+		end
+		expect(page.body).to have_text('successfully')
+	end
+
 end
