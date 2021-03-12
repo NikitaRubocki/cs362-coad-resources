793631c32cee4c3b286c5b5f3bc2c3e7e393642e
Nikita Rubocki <nikita.rubocki@gmail.com>
2021-03-11 16:48:18 -0800

    Add feature test for closing a ticket

--- spec/features/tickets/close_ticket_spec.rb
+++ spec/features/tickets/close_ticket_spec.rb
@@ -2,4 +2,15 @@ require 'rails_helper'
 
 RSpec.describe 'Closing a ticket', type: :feature do
 
+	let(:admin) { create(:user, :admin_role) }
+
+  it 'closes a ticket' do
+		ticket = create(:ticket)
+		log_in_as(admin)
+		click_on ticket.name
+		click_on 'Close'
+		click_on ticket.name
+		expect(page.body).to have_selector('dd', text: 'Closed')
+	end
+
 end
