fa3cee99cb020d70777f1eec9c58ce204ffef083
Nikita Rubocki <nikita.rubocki@gmail.com>
2021-03-11 16:48:09 -0800

    Add feature test for deleting a ticket

--- spec/features/tickets/admin_deletes_ticket_spec.rb
+++ spec/features/tickets/admin_deletes_ticket_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe 'Deleting a Ticket', type: :feature do
+  
+	let(:admin) { create(:user, :admin_role) }
+
+  it 'deletes a ticket' do
+		ticket = create(:ticket)
+		log_in_as(admin)
+		click_on ticket.name
+		click_on 'Delete'
+		expect(page.body).to have_text('deleted')
+	end
 
 end
