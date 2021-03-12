91cc752c29e05b9487328b23f7dddbff715fea25
Nikita Rubocki <nikita.rubocki@gmail.com>
2021-03-11 17:29:30 -0800

    Add feature test for capturing tickets

--- spec/features/tickets/capture_ticket_spec.rb
+++ spec/features/tickets/capture_ticket_spec.rb
@@ -2,4 +2,22 @@ require 'rails_helper'
 
 RSpec.describe 'Capturing a ticket', type: :feature do
 
+	let(:org) { create(:organization, :approved) }
+	let(:user) { create(:user, :org_role, organization: org) }
+
+	describe 'user with approved organization' do
+		it 'closes a ticket' do
+			ticket = create(:ticket)
+			log_in_as(user)
+			click_on 'Tickets'
+			click_on ticket.name
+			click_on 'Capture'
+			click_on 'Tickets'
+			click_on ticket.name
+			click_on 'Close'
+			click_on ticket.name
+			expect(page.body).to have_selector('dd', text: 'Closed')
+		end
+	end
+
 end
