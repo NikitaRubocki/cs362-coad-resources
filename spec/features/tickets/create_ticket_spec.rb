0f43f3e4165b3e66b6033551a27d24a0f6d5c5a4
Nikita Rubocki <nikita.rubocki@gmail.com>
2021-03-11 17:49:53 -0800

    Add feature test for creating a ticket

--- spec/features/tickets/create_ticket_spec.rb
+++ spec/features/tickets/create_ticket_spec.rb
@@ -2,4 +2,18 @@ require 'rails_helper'
 
 RSpec.describe 'Creating a Ticket', type: :feature do
 
+	it 'creates a ticket' do
+		region = create(:region)
+		category = create(:resource_category)
+		visit root_path
+		click_on 'Get Help'
+		fill_in 'Full Name', with: 'Fake Person'
+		fill_in 'Phone Number', with: '5414567890'
+		select region.name, from: 'Region'
+		select category.name, from: 'Resource Category'
+		fill_in 'Description', with: 'Lorem ipsom'
+		click_on 'Send this help request'
+		expect(page.body).to have_text('Submitted')
+	end
+
 end
