c5518ed568de7956501ad5747b1b6bacd8654f12
Nikita Rubocki <nikita.rubocki@gmail.com>
2021-03-14 09:31:26 -0700

    Change approve to reject

--- spec/features/organizations/reject_organization_spec.rb
+++ spec/features/organizations/reject_organization_spec.rb
@@ -4,13 +4,13 @@ RSpec.describe 'Rejecting an organization', type: :feature do
 
   let(:admin) { create(:user, :admin_role) }
 
-  it 'approves an organization' do
+  it 'rejects an organization' do
     org = create(:organization, :submitted)
     log_in_as(admin)
 		click_on 'Organizations'
     click_on org.name
-    click_on 'Approve'
-    expect(page.body).to have_text('approved')
+    click_on 'Reject'
+    expect(page.body).to have_text('rejected')
   end
 
 end
