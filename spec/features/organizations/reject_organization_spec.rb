448b9e590ca31a66bf2c7ec2735160329c6b81e7
Nikita Rubocki <nikita.rubocki@gmail.com>
2021-03-13 18:02:05 -0800

    Add feature tests for organization rejection

--- spec/features/organizations/reject_organization_spec.rb
+++ spec/features/organizations/reject_organization_spec.rb
@@ -2,4 +2,15 @@ require 'rails_helper'
 
 RSpec.describe 'Rejecting an organization', type: :feature do
 
+  let(:admin) { create(:user, :admin_role) }
+
+  it 'approves an organization' do
+    org = create(:organization, :submitted)
+    log_in_as(admin)
+		click_on 'Organizations'
+    click_on org.name
+    click_on 'Approve'
+    expect(page.body).to have_text('approved')
+  end
+
 end
